fbd8a6c1202f7a15ac0598eb71edb84b24ec50c1
Daniel Kelly <me@danielkelly.io>
2025-09-02 15:37:13 -0500

    3-imports-in-nuxt-layers

--- .playground/app.vue
+++ .playground/app.vue
@@ -1,3 +1,6 @@
+<script setup lang="ts">
+import HelloWorld from "#layers/my-layer/components/HelloWorld.vue";
+</script>
 <template>
   <h1>Hello from the end application</h1>
   <HelloWorld />
--- app.vue
+++ app.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import HelloWorld from "#layers/my-layer/components/HelloWorld.vue";
+</script>
 <template>
+  <h1>Hello from the end application</h1>
   <HelloWorld />
 </template>
